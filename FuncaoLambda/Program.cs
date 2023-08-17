01c24bfe8baf8cf770b7a06bce1f813143d92ef9
Renatatr <renatatrintin@gmail.com>
2023-08-17 11:53:10 -0300

    função lambda exemplo

---
+++ FuncaoLambda/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("digite 2 numeros: ");
+int numero1 = int.Parse(Console.ReadLine());
+int numero2 = int.Parse(Console.ReadLine());
+
+int Somar(int a, int b) => a + b;
+
+Console.WriteLine(Somar(numero1,numero2));
